d7d7cbb5b13967e1d183fa7d8762235d2b19c966
AndreBilley <116080366+AndreBilley@users.noreply.github.com>
2023-03-12 22:55:44 +0000

    Update Nea game V2.py

--- Nea game V2.py
+++ Nea game V2.py
@@ -19,7 +19,6 @@ tile_size = 40
 bg_img = pygame.image.load('Nea_game_files/glacial_mountains.png')
 grnd_img = pygame.image.load('Nea_game_files/ground.png')
 bg_img = pygame.transform.scale(bg_img, (800,800))
-# grnd_img = pygame.transform.scale(grnd_img, (800,400))
 
 ############-Draw Grid-###############
 def draw_grid():
@@ -30,24 +29,28 @@ def draw_grid():
 class Player():
     def __init__(self, x, y):
         self.images_right = []
+        self.images_left = []
         self.index = 0
         self.counter = 0
         # char = pygame.image.load('Nea_game_files/Sprites/adventurer-idle-0.png')
         for num in range (0, 5):
             char_right = pygame.image.load(f'Nea_game_files/Sprites/adventurer-run-{num}.png')
             char_right = self.image = pygame.transform.scale(char_right, (100,80))
+            char_left = pygame.transform.flip(char_right, True, False)
             self.images_right.append(char_right)
+            self.images_left.append(char_left)
         self.image = self.images_right[self.index] 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
         
     def update(self):
         dx,dy = 0,0
-        walk_cooldown = 4
+        walk_cooldown = 3
         
 ################## -Controls- ##################
         key = pygame.key.get_pressed()
@@ -56,11 +59,21 @@ class Player():
         ###### -Forward- ######
         if key[pygame.K_RIGHT] or key[pygame.K_d]:
             self.counter +=1
+            self.direction = 1
             dx += 5
+            
+        if key[pygame.K_RIGHT] == False and key[pygame.K_LEFT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]  
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
         
         ###### -Backward- ######
         if key[pygame.K_LEFT] or key[pygame.K_a]:
             self.counter +=1
+            self.direction = -1
             dx -= 5
 
         
@@ -84,13 +97,15 @@ class Player():
         #~~~~~~~~~~~~~~~~~#    
         
         ###### -Animations- ######
-        # if self.counter > walk_cooldown:
-        #     self.counter = 0
-        self.index += 1
-        if self.index >= len(self.images_right):
-            self.index = 0
-        self.image = self.images_right[self.index]  
-           
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]  
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
            
            
         dy += self.vel_y
